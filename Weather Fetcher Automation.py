a0719672e2abebdebb410e46a8374f7681e100e8
Ioannis Xenofontos <108096250+Prisual@users.noreply.github.com>
2022-06-24 21:49:07 -0400

    Add files via upload

---
+++ Weather Fetcher Automation.py
@@ -0,0 +1,20 @@
+import requests
+
+API_KEY = "insert api key from OpenWeatherMap"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] * 1.8 - 459.67)
+    feels_like = round(data["main"]["feels_like"] * 1.8 - 459.67)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "°F")
+    print("It Feels Like:", feels_like, "°F")
+else:
+    print("not valid, Please try again.")
